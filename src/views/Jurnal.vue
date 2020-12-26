f6a4fd0bf57796c7abc507df50ecf8d86561ce24
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-12-26 16:16:34 +0700

    add desc to jurnal

--- src/views/Jurnal.vue
+++ src/views/Jurnal.vue
@@ -3,7 +3,7 @@
     <section class="dark:bg-gray-900 dark:text-white py-10 border-gray-900">
       <div class="container mx-auto px-4">
         <h1 class="text-4xl font-bold">Jurnal</h1>
-        <p class="text-medium mt-4">Ini adalah halaman jurnal, kamu bisa pilih jurnal kamu lewat minat</p>
+        <p class="text-medium mt-4 lg:w-1/2">Ini adalah halaman jurnal, kamu bisa pilih jurnal kamu lewat minat. Disini kita akan praktik membuat sesuatu.</p>
         <div class="flex w-full md:w-2/3 items-center flex-wrap mt-4">
           <select v-model="selected" class="bg-gray-300 px-4 py-2 w-2/3 rounded dark:text-black appearance-none">
             <option value="">--Pilih Minat--</option>
